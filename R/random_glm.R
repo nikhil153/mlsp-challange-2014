fbf0d6653c0a9209c82a7362a2541610c5a6880f
Jon Pipitone <jon.pipitone@gmail.com>
2014-07-09 23:16:06 -0400

    RF with graph metrics + rf feature selection

--- R/random_glm.R
+++ R/random_glm.R
@@ -1,20 +1,24 @@
 #!/usr/bin/env R
 # Toy example to generate a random forest over the dataset
 
-library(randomForest)
-library(miscTools)
 library(plyr)
+library(reshape)
+library(miscTools)
 library(randomGLM)
+library(randomForest)
 
 # load data
-train.fnc = read.csv('contest-data/train_FNC.csv')
-train.labels = read.csv('contest-data/train_labels.csv')
-train.sbm = read.csv('contest-data/train_SBM.csv')
-train.labels <- factor(train.labels$Class)
+train.fnc     = read.csv('contest-data/train_FNC.csv')
+train.graph   = read.csv('contest-data/train_FNC_graph.csv', header=F)
+train.labels  = read.csv('contest-data/train_labels.csv')
+train.sbm     = read.csv('contest-data/train_SBM.csv')
+colnames(train.graph)[1] = "Id"
+train.labels$Class = factor(train.labels$Class)
 
 # merge and clean up data
-train.data = merge(train.fnc, train.sbm)
-train.all = merge(train.data, train.labels)
+train.data    = merge(train.fnc, train.sbm)
+train.data    = merge(train.data, train.graph)
+train.all     = merge(train.data, train.labels)
 
 #test.fnc = read.csv('contest-data/test_FNC.csv')
 #test.sbm = read.csv('contest-data/test_SBM.csv')
@@ -22,26 +26,25 @@ train.all = merge(train.data, train.labels)
 
 set.seed(0)
 
-
+print('training...')
 # predictions for testing data
-test.fnc = read.csv('contest-data/test_FNC.csv')
-test.sbm = read.csv('contest-data/test_SBM.csv')
-test.data = merge(test.fnc, test.sbm)
 
 RGLM = randomGLM(train.data, 
-                 train.labels,
-                 test.data,
-                 classify=TRUE, keepModels=TRUE, nThreads=8)
+                 train.labels$Class,
+                 nBags=200,
+                 maxInteractionOrder = 2,
+          #       test.data,
+                 classify=TRUE, keepModels=TRUE, nThreads=16)
 
 predictedOOB = RGLM$predictedOOB
-t=table(train.labels, predictedOOB)
+t=table(train.labels$Class, predictedOOB)
 print(t)
-train.error = round((t[1,2]+t[2,1])/length(train.labels)*100, 4)
+train.error = round((t[1,2]+t[2,1])/length(train.labels$Class)*100, 4)
 print(paste("train error rate = ", train.error))
 
-predictions = as.data.frame(RGLM$predictedTest.response)
-submission  = data.frame(Id=test.data$Id, Probability = predictions$"1")
-write.csv(submission, file="predictions.csv")
+#predictions = as.data.frame(RGLM$predictedTest.response)
+#submission  = data.frame(Id=test.data$Id, Probability = predictions$"1")
+#write.csv(submission, file="predictions.csv")
 
 
 # variable importance measures
--- R/randomforest.R
+++ R/randomforest.R
@@ -11,31 +11,43 @@ library(plyr)
 train.fnc = read.csv('contest-data/train_FNC.csv')
 train.labels = read.csv('contest-data/train_labels.csv')
 train.sbm = read.csv('contest-data/train_SBM.csv')
+train.graph   = read.csv('contest-data/train_FNC_graph.csv')
 train.labels$Class <- factor(train.labels$Class)
 
 # merge and clean up data
-train.data = merge(train.fnc, train.sbm)
-train.all = merge(train.data, train.labels)
-
-train.fnc$Id <- NULL   # assume sorted
-train.labels$Id <- NULL
-train.sbm$Id <- NULL
-train.data$Id <- NULL
-train.all$Id <- NULL
+train.data    = merge(train.fnc, train.sbm)
+train.data    = merge(train.data, train.graph)
+train.all     = merge(train.data, train.labels)
 
 set.seed(0)
 
+# first pass with all the data
 clf = randomForest(Class ~ ., data=train.all, ntree=10000)
-subset(as.data.frame(importance(clf)), MeanDecreaseGini < 0.02) # important features
-varImpPlot(clf)
-print(clf)
+
+# second pass with just the important data
+gini_threshold = 0.1
+features = row.names(subset(imp, MeanDecreaseGini > gini_threshold))
+clf = randomForest(Class ~ ., data=train.all[, append(features, "Class")], ntree=10000)
 
 # predictions for testing data
 test.fnc = read.csv('contest-data/test_FNC.csv')
 test.sbm = read.csv('contest-data/test_SBM.csv')
+test.graph   = read.csv('contest-data/test_FNC_graph.csv')
 test.data = merge(test.fnc, test.sbm)
+test.data = merge(test.data, test.graph)
 
-# predict on dataset (with Id column removed)
-predictions = as.data.frame(predict(clf, test.data[,2:ncol(test.data)], type="prob"))
+# predict on dataset
+predictions = as.data.frame(predict(clf, test.data[,append(features, "Class")], type="prob"))
 submission = data.frame(Id=test.data$Id, Probability = predictions$"1")
 write.csv(submission, file="predictions.csv")
+
+#clf = randomForest(Class ~ ., data=train.all, ntree=10000)
+#imp = as.data.frame(importance(clf))
+#for (gini_threshold in seq(0.075,0.15,0.01)) { 
+#	features = row.names(subset(imp, MeanDecreaseGini > gini_threshold))
+#	clf = randomForest(Class ~ ., data=train.all[, append(features, "Class")], ntree=10000)
+#	print(gini_threshold)
+#	print(clf)
+#}
+
+
